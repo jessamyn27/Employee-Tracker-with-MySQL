f8afc58d2fd9356ae09733fb79c433bb62f0bda7
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2021-07-28 22:11:36 -0500

    got add employee to work and function to start after every query

--- server.js
+++ server.js
@@ -18,40 +18,138 @@ const db = mysql.createConnection({
     },
     console.log(`Connected to the database.`)
 );
+const start = () => {
+    inquirer.prompt([{
+            type: 'list',
+            message: "what would you like to do?",
+            name: 'answer',
+            choices: [
+                'view employees',
+                'add new employees',
+                'update employee role',
+                'view roles',
+                'add new role',
+                'view departments',
+                'add new departments',
+                'quit'
+            ]
+        }])
+        .then((response) => {
+            console.log(response)
+            switch (response.answer) {
+                case "view employees":
+                    db.query(`SELECT * FROM employees;`, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        console.table(result);
+                        start();
+                    });
+                    break;
+                case "view roles":
+                    db.query(`SELECT * FROM roles;`, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        console.table(result);
+                        start();
+                    });
+                    break;
+                case "view departments":
+                    db.query(`SELECT * FROM department;`, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        console.table(result);
+                        start();
+                    });
+                    break;
+                case "add new employees":
+                    addNewEmp()
 
-inquirer.prompt([{
-        type: 'list',
-        message: "what would you like to do?",
-        name: 'answer',
-        choices: [
-            'view employees',
-            'add new employees',
-            'update employee role',
-            'view roles',
-            'add new role',
-            'view departments',
-            'add new departments',
-            'quit'
-        ]
-    }])
-    .then((response) => {
-        console.log(response)
-        switch (response.answer) {
-            case "view employees":
-                db.query(`SELECT * FROM employees;`, (err, result) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    console.table(result);
-                });
+                    break;
+            }
+        })
 
-        }
-    })
+}
 
-// view all employees
 
 // add new employees - first name - last name - role - manager
-// update employee role - list of existing names to choose - role to assign to name from list of roles - 
+const addNewEmp = () => {
+    inquirer.prompt([{
+        type: 'input',
+        message: "employee first name?",
+        name: 'first'
+    }, {
+        type: 'input',
+        message: "employee last name?",
+        name: 'last'
+
+    }, {
+        type: 'list',
+        message: "employee role?",
+        name: 'role',
+        choices: [
+            'sales lead',
+            'salesperson',
+            'lead engineer',
+            'software engineer',
+            'account manager',
+            'accountant',
+            'legal team lead',
+            'lawyer'
+        ]
+    }, {
+        type: 'list',
+        message: "employee manager?",
+        name: 'manager',
+        choices: [
+            'john doe',
+            'ashley rodriguez',
+            'kunal singh',
+            'sarah lourd'
+        ]
+    }, ]).then(async(response) => {
+        let first = response.first;
+        let last = response.last;
+        let role = response.role;
+        let manager = response.manager;
+        // here i need to convert role and manager into their corr id's
+        let roleId = await convertRoleToId(role)
+        let mgrId = await convertMgrToId(manager)
+
+        db.query(`INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+        VALUES
+        (?,?,?,?);`, [first, last, roleId, mgrId], (err, results) => {
+            if (err) throw err
+            console.table(`success! you added ${first} ${last} with the role of ${role} and the manager is ${manager}`)
+            start();
+        })
+    });
+
+}
+
+const convertMgrToId = (mgr) => {
+    return new Promise(function(resolve, reject) {
+        db.query(`SELECT * FROM employees WHERE CONCAT(first_name, ' ', last_name) LIKE '%${mgr}%';`, function(err, results) {
+            if (err) throw err
+
+            let id = results[0].id
+            resolve(id);
+        })
+    })
+}
+
+const convertRoleToId = (role) => {
+        return new Promise(function(resolve, reject) {
+            db.query(`SELECT * FROM roles WHERE title LIKE '%${role}%';`, function(err, results) {
+                if (err) throw err
+                let id = results[0].id
+                resolve(id);
+            })
+        })
+    }
+    // update employee role - list of existing names to choose - role to assign to name from list of roles - 
 
 // view all roles  
 // add new role -  role name - salary - dept
@@ -62,7 +160,7 @@ inquirer.prompt([{
 // quit 
 
 // })
-
+start();
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
